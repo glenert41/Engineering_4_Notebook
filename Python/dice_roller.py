3bb82b6ffc17fbef306523c582fa1179d896af50
glenert41 <48455265+glenert41@users.noreply.github.com>
2020-10-29 14:43:37 -0400

    Update dice_roller.py

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -5,17 +5,17 @@ from random import randint
 import sys 
 import time
 
-print ("Automatic Dice Roller")
+print ("Automatic Dice Roller") 	#Show the Program is running
 
 
-while True:
-	time.sleep(1)
-	RollPrompt = input("Press Enter to Roll and x to quit: ")
+while True:			#This while loop continues to run, allowing for multiple inputs 
+	time.sleep(1)		#Just a delay to not overwhelm the Pi
+	RollPrompt = input("Press Enter to Roll and x to quit: ")	#asks user for input, stores as variable
 	
 	if RollPrompt == (""):
-		print(randint(1,6))
-	if RollPrompt == ("x"): 
-                sys.exit()	
+		print(randint(1,6))	#if the input is enter (empty), then choose random number 1-6
+	if RollPrompt == ("x"): 	
+                sys.exit()		#if the input is x, quit the program
 		
 
 
