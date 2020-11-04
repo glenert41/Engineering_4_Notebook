4acda61c9a1c017a2313a6637800b27d75a0e337
glenert41 <glenert41@charlottesvilleschools.org>
2020-11-04 18:09:08 +0000

    day 1 calc

---
+++ Python/calculator.py
@@ -0,0 +1,12 @@
+#This is a Python Calculator
+#This is by Graham Lenert
+
+inputNum1 = input("Press first number: ")
+inputNum2 = input("Press second number: ")
+
+
+def doMath(inputNum1,inputNum2,operator):
+	if operator == 1:
+		return int(inputNum1) + int(inputNum2)
+
+print("Sum:\t\t" + str(doMath(inputNum1,inputNum2,1)))
