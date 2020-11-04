003b2914363d13d1f15aaa5aee916291ceec0bd7
glenert41 <glenert41@charlottesvilleschools.org>
2020-11-04 18:14:20 +0000

    added difference

--- Python/calculator.py
+++ Python/calculator.py
@@ -8,5 +8,9 @@ inputNum2 = input("Press second number: ")
 def doMath(inputNum1,inputNum2,operator):
 	if operator == 1:
 		return int(inputNum1) + int(inputNum2)
+	if operator == 2:
+		return int(inputNum1) - int(inputNum2)
+
 
 print("Sum:\t\t" + str(doMath(inputNum1,inputNum2,1)))
+print("Difference:\t\t" + str(doMath(inputNum1,inputNum2,2)))
