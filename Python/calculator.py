c021fb4d5ef038f84b33e04ec031e63ced21c1eb
glenert41 <glenert41@charlottesvilleschools.org>
2020-11-09 17:41:17 +0000

    added Mod

--- Python/calculator.py
+++ Python/calculator.py
@@ -16,9 +16,11 @@ def doMath(inputNum1,inputNum2,operator):
 		Q = int(inputNum1) / int(inputNum2)
 		RQ = round(Q,2)
 		return RQ
-
+	if operator == 5:
+		return int(inputNum1) % int(inputNum2)
 
 print("Sum:\t\t" + str(doMath(inputNum1,inputNum2,1)))
 print("Difference:\t" + str(doMath(inputNum1,inputNum2,2)))
 print("Prodcut:\t" + str(doMath(inputNum1,inputNum2,3)))
 print("Quotient:\t" + str(doMath(inputNum1,inputNum2,4)))
+print("Modulo:\t" + str(doMath(inputNum1,inputNum2,5)))
