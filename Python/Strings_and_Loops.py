a1712d7ec126e00ebc91cb8562fc6a633689ae7e
Graham Lenert <glenert41@charlottesvilleschools.org>
2020-11-17 02:31:54 +0000

    added comments

--- Python/Strings_and_Loops.py
+++ Python/Strings_and_Loops.py
@@ -2,13 +2,13 @@
 #In this program, the user inputs a sentence, and then an it gets printed back letter for letter, with a minus as a space
 #Graham Lenert
 
-input = str(input("Enter your phrase: ")) 
-input = input.replace(" ", "-") 
+input = str(input("Enter your phrase: ")) #takes the input, and converts it to a string
+input = input.replace(" ", "-")  #replaces all spaces with a - 
 
-input_list = list(input) 
-input_list.append("-") 
+input_list = list(input)  #convertst the string to a list, in order to be printed with the correct syntax
+input_list.append("-")  #adds a - to the end of the list 
 
-print(*input_list, sep='\n')
+print(*input_list, sep='\n') #prints the list, place by place, and returns each line
 
 
   
