167bc17b5e71e7c26eb87a521c76144699f34960
glenert41 <glenert41@charlottesvilleschools.org>
2020-12-28 21:08:56 +0000

    added comments

--- Python/led_blink.py
+++ Python/led_blink.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setwarnings(False) # sets weird warnings to false
-leds = [38] # list with LED pins
+leds = [38] #  put the physical pin number here
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(38, GPIO.OUT) # sets board pins 38 and 40 to output
+GPIO.setup(leds, GPIO.OUT) # sets the pins to output
 
-for i in range(0, 3): # blinks the LED's 10 times
-	GPIO.output(38, 1)
+for i in range(0, 3): # blinks the LED(s) 3 times
+	GPIO.output(leds, 1)
 	time.sleep(0.5)
-	GPIO.output(38, 0)
+	GPIO.output(leds, 0)
 	time.sleep(0.5)
 	print("hi")
