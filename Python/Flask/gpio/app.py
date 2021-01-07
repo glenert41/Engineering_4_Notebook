8575be8002c63effc13af87f0eef31f87b15f6ef
glenert41 <glenert41@charlottesvilleschools.org>
2021-01-07 16:01:40 +0000

    On/Off works; looks boring

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -9,14 +9,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-     if request.method == "POST":
-          if request.form['submitBtn1] == "Do Button 1":
-              GPIO.output(17,GPIO.HIGH)
-              msg = request.form.get("submitBtn")
-     else:
-          GPIO.output(17,GPIO.LOW)
-          msg = "No click yet."
-     return render_template("index.html", msg=msg)
+	print(request.method)
+        if request.method == 'POST':
+            if request.form.get('button1') == 'button1':
+                # pass
+               	print("button1")
+		GPIO.output(17,GPIO.HIGH)
+
+            elif  request.form.get('button2') == 'button2':
+                # pass # do something else
+                print("button2")
+		GPIO.output(17,GPIO.LOW)
+
+            else:
+                # pass # unknown
+                return render_template("index.html")
+
+        elif request.method == 'GET':
+            # return render_template("index.html")
+            print("No Post Back Call")
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
