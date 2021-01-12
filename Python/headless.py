671ed74c7deef02fc5944c23c1dc54a9ccf5f914
glenert41 <glenert41@charlottesvilleschools.org>
2021-01-12 16:01:28 +0000

    it is graphing

--- Python/headless.py
+++ Python/headless.py
@@ -1,6 +1,7 @@
 import time
 import Adafruit_SSD1306
 import Adafruit_LSM303
+import math
 
 from PIL import Image
 from PIL import ImageDraw
@@ -28,17 +29,35 @@ bottom = height - padding
 x = padding
 font = ImageFont.load_default()
 
+x_Changer = 0
 
 while True:
-	
+
+	#this moves the x value of the point printed along the screen
+	x_Value = x + x_Changer
+
+
 	accel, mag = accelerometer.read() # gets accelerometer data
 	accel_x, accel_y, accel_z = accel #sets the acceleration values
 	mag_x, mag_y, mag_z = mag #although I don't use this, the .read() taken 6 points of data, so you need to give places for all 6 data points
 
-	draw.text((x, top), "Accelerometer Data:", font=font, fill=255) # draws header
+	#change this to change the data you want plotted	
+	#this works best for graphing the Z acceleration
+	#dataValue = abs(accel_z)/50
 
+	dataValue = accel_x/10
+	print(dataValue)
+
+
+
+	draw.text((x, top), "Accelerometer Data:", font=font, fill=255) # draws header
+	draw.text((x_Value, top + dataValue),"-", font=font, fill=255) 
 
 
 	#actually writes to the display
 	disp.image(image) # displays x, y, z, and header
 	disp.display()
+
+	x_Changer = x_Changer + 1
+
+	time.sleep(.1)
